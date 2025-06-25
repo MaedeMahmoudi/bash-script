d0aa343f3f0cdf95ac2f104e9400a726d7905289
Maedeh Mahmoudi <maedeemahmoudi5@gmail.com>
2025-06-25 15:54:27 +0330

    Access Arguments

---
+++ 12_acessArguments.sh
@@ -0,0 +1,11 @@
+
+  #!/bin/bash
+
+  echo "script name : $0"
+  echo "first argument : $1"
+  echo "second argument : $2"
+  echo "third argument : $3"
+  echo "number of arguments : $#"
+  echo "all arguments (as one string): $*"
+  echo "all arguments (as separate items): $@"
+
