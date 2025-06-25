ece72e2c9eeb826b7eeae315e08d7c56ab8c18b5
Maedeh Mahmoudi <maedeemahmoudi5@gmail.com>
2025-06-25 22:18:23 +0330

    Combining Multiple Conditions

---
+++ 18_combiningCondition.sh
@@ -0,0 +1,12 @@
+
+
+  #!bin/bash/
+
+  read -p "Enter first number = " a
+  read -p "Enter second number = " b
+  
+    if [ "$a" -gt 10 ] && [ "$b" -lt 5 ]; then
+ 
+        echo "both conditions are true." 
+
+    fi
