5c91e54d79183024197b58982e4a07cc61930999
Maedeh Mahmoudi <maedeemahmoudi5@gmail.com>
2025-06-26 17:36:57 +0330

    If Else Example 2

---
+++ 20_ifElseExample.sh
@@ -0,0 +1,18 @@
+
+  #!bin/bash/
+
+  read -p "Enter a number to check: " number
+
+   if [ "$number" -gt 0 ]; then
+
+      echo "Number is positive"
+
+   elif [ "$number" -lt 0 ]; then
+
+      echo "Number is negetive"
+
+   else
+
+      echo "Number is zero"
+
+   fi
